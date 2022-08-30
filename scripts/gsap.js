914c94e1db74dd50d01bf4235155fd2f3d025c56
Lacy Morrow <me@lacymorrow.com>
2022-08-30 11:00:44 -0400

    save

--- scripts/gsap.js
+++ scripts/gsap.js
@@ -25,10 +25,11 @@ heroTimeline
 .call(removeHover)
 .to(".magic-venn-bottom-right", { scale: 10, rotation: 50, autoAlpha: 0, ease: "back.in(0.5)", duration: 20 }, "-=15")
 .to(".magic-venn-bottom-left", { scale: 10, rotation: 70, autoAlpha: 0, ease: "back.in(0.5)", duration: 20 }, "-=15")
-// .call(isDottted)
+.call(isDottted)
 .to(".hero-link", { autoAlpha: 0, ease: "back.in(0.5)", duration: 20 }, "-=15")
 .to(".magic-venn-top-right", { scale: 10, rotation: 110, autoAlpha: 0, ease: "back.in(0.5)", duration: 20 }, "-=15")
 .to(".magic-venn-top-left", { scale: 10, rotation: 130, autoAlpha: 0.3, y: "50vh", ease: "back.in(0.5)", duration: 40 }, "-=15")
+.to(".back-to-top", {color: 'purple', opacity: '1'})
 .set(".theme", { className: "theme is-dark"}, "<")
 
 const whatTimeline = gsap.timeline({
@@ -36,10 +37,8 @@ const whatTimeline = gsap.timeline({
   id: "venn2",
   trigger: ".js-section-whatis",
   pin: true,
-  scrub: 2,
+  scrub: true,
 }});
-// const isDottted = () => document.querySelector(".magic-venn-top-left").classList.toggle("is-dotted")
-// const removeHover = () => document.querySelectorAll(".magic-venn-circle").forEach(el => el.classList.toggle("has-no-hover"))
 
 whatTimeline
 .to(".whatis-left", { scale: 5, autoAlpha: 0, rotation: 80, x: "-50vw", y: "-50vh", ease: "back.in(0.5)", duration: 40 })
@@ -56,7 +55,7 @@ var valuesSections = gsap.utils.toArray('.js-section-values')
 
 valuesSections.forEach(function (section, i) {
   const randomPosOrNegInteger = () => Math.ceil(Math.random() * 50) * (Math.round(Math.random()) ? 1 : -1);
-  gsap.timeline().fromTo(section.querySelectorAll(".js-values-card"), { 
+  gsap.fromTo(section.querySelectorAll(".js-values-card"), { 
     scale: 0.8, 
     autoAlpha: 0, 
     y: randomPosOrNegInteger,
@@ -82,8 +81,8 @@ valuesSections.forEach(function (section, i) {
       start: "top 10%",
       end: () => `+=${section.offsetHeight}`,
       onEnter: () => document.querySelector(".theme").className = "theme is-dark",
-      onLeaveBack: () =>  document.querySelector(".theme").className = "theme is-light",
-      pin: true,
+      onEnterBack: () => document.querySelector(".theme").className = "theme is-dark",
+      // pin: true,
       scrub: 0.5,
     }
   })
@@ -91,7 +90,7 @@ valuesSections.forEach(function (section, i) {
 
 var twilionSections = gsap.utils.toArray('.js-section-twilion')
 twilionSections.forEach(function (section, i) {
-  gsap.timeline().fromTo(section.querySelectorAll(".js-twilion-header"), { 
+  gsap.fromTo(section.querySelectorAll(".js-twilion-header"), { 
     scale: 0.8, 
     autoAlpha: 0 
   }, {  
@@ -103,33 +102,52 @@ twilionSections.forEach(function (section, i) {
       // markers: true,
       id: `twilion-${i}`,
       trigger: section,
-      start: "top center",
-      end: 'top bottom',
       onEnter: () => document.querySelector(".theme").className = "theme is-red",
-      // pin: true,
+      onLeaveBack: () => document.querySelector(".theme").className = "theme is-red",
+      onEnterBack: () => document.querySelector(".theme").className = "theme is-red",
+      pin: true,
+      scrub: true,
     }
   })
 });
 
 const missingTimeline = gsap.timeline({
   scrollTrigger: {
-    id: "missing",
     trigger: ".section-missing",
     pin: true,
     scrub: true,
-    start: "top top",
-    end: "bottom bottom",
     onEnter: () => document.querySelector(".theme").className = "theme is-blue",
-    onEnterBack: () => document.querySelector(".theme").className = "theme is-dark",
     onLeaveBack: () => document.querySelector(".theme").className = "theme is-blue",
-    onLeave: () => document.querySelector(".theme").className = "theme",
+    onEnterBack: () => document.querySelector(".theme").className = "theme is-bluer",
   }
 });
 
 missingTimeline
-.from(".missing-left", { scale: 5, autoAlpha: 0, rotation: 80, x: "-50vw", y: "-50vh", ease: "back.in(0.5)", duration: 4 })
-.call(isBlue)
-
+.to(".js-missing-0", {opacity: '0', duration: 50})
+.fromTo(".js-missing-1", {opacity: '0', y: '100px', ease: "power4.in"}, {opacity: '1', duration: 5, ease: "power4.out"})
+.fromTo(".js-missing-1", {opacity: '1', y: '-100px'}, {opacity: '0', duration: 5})
+.set(".theme", { className: "theme is-bluer"}, ">")
+.fromTo(".missing-venn-bottom-right", {opacity: '1'}, {opacity: '0.5', duration: 5})
+.fromTo(".js-missing-2", {opacity: '0', duration: 5}, {opacity: '1', duration: 5})
+.delay(20)
+.fromTo(".js-missing-2", {opacity: '1', duration: 5}, {opacity: '0', duration: 5})
+.fromTo(".missing-venn-bottom-right", {opacity: '0.5'}, {opacity: '1', duration: 5})
+.fromTo(".missing-venn-bottom-left", {opacity: '1'}, {opacity: '0.5', duration: 5})
+.fromTo(".js-missing-3", {opacity: '0', duration: 5}, {opacity: '1', duration: 5})
+.delay(20)
+.fromTo(".js-missing-3", {opacity: '1', duration: 5}, {opacity: '0', duration: 5})
+.fromTo(".missing-venn-bottom-left", {opacity: '0.5'}, {opacity: '1', duration: 5})
+.fromTo(".missing-venn-top-right", {opacity: '1'}, {opacity: '0.5', duration: 5})
+.fromTo(".js-missing-4", {opacity: '0', duration: 5}, {opacity: '1', duration: 5})
+.delay(20)
+.fromTo(".js-missing-4", {opacity: '1', duration: 5}, {opacity: '0', duration: 5})
+.fromTo(".missing-venn-top-right", {opacity: '0.5'}, {opacity: '1', duration: 5})
+.fromTo(".missing-venn-top-left", {opacity: '1'}, {opacity: '0.5', duration: 5})
+.fromTo(".js-missing-5", {opacity: '0', duration: 5}, {opacity: '1', duration: 5})
+.delay(20)
+.fromTo(".js-missing-5", {opacity: '1', duration: 5}, {opacity: '0', duration: 5})
+.fromTo(".missing-venn-top-left", {opacity: '0.5'}, {opacity: '1', duration: 5})
+.set(".theme", { className: "theme is-light"}, ">")
 
 const livingTimeline = gsap.timeline();
 const toggleBackgrounds = () => document.querySelectorAll(".has-image").forEach(el => el.classList.toggle("is-completed"))
@@ -139,8 +157,8 @@ livingTimeline
 .from(".atropos-magic-venn-top-left", { autoAlpha: 0, x: "-50px", y: "-50px", ease: "power3.out", duration: 4 })
 .from(".atropos-magic-venn-top-right", { autoAlpha: 0, x: "50px", y: "-50px", ease: "power3.out", duration: 4 })
 .from(".atropos-magic-venn-bottom-left", { autoAlpha: 0, x: "-50px", y: "50px", ease: "power3.out", duration: 4 })
+.from(".atropos-magic-venn-bottom-right", { autoAlpha: 0, x: "50px", y: "50px", ease: "power3.out" })
 .call(toggleBackgrounds)
-.from(".atropos-magic-venn-bottom-right", { autoAlpha: 0, x: "50px", y: "50px", ease: "power3.out", duration: 4 })
 
 ScrollTrigger.create({
   id: "living",
